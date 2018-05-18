e24bca1a1f746088bc8a3cc45907550fc87292d9
BenoitBlondel <benoit-blondel@hotmail.fr>
2018-05-18 13:00:10 +0200

    Je sais pas pourquoi eclipse à supprimer ICustomerDao lors du pull ... donc je l'ai remis.

--- webServiceProxiBanque/src/main/java/fr/gtm/pbsi/dao/ICustomerDao.java
+++ webServiceProxiBanque/src/main/java/fr/gtm/pbsi/dao/ICustomerDao.java
@@ -4,13 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.gtm.pbsi.domain.Customer;
 
-
 /**
- * @author Bovin Blondel Demolis Colbert Sersoub
- *  * Interface permettant de définir un Repository Spring. Nous utilisons ici
- * l'interface JpaRepository de Spring pour demander au Framework de fabriquer
- * un DAO évolué qui manipule une entité JPA/Hibernate.
+ * @author Bovin Blondel Demolis Colbert Sersoub * Interface permettant de
+ *         definir un Repository Spring. Nous utilisons ici l'interface
+ *         JpaRepository de Spring pour demander au Framework de fabriquer un
+ *         DAO evolue qui manipule une entite JPA/Hibernate.
  */
-public interface ICustomerDao extends JpaRepository<Customer,Integer> {
+public interface ICustomerDao extends JpaRepository<Customer, Integer> {
 
 }
