f082339c765e0c5d2bd0050e3f5e5eb0fadb13fd
MehdiColbert <mehdi.colbert@gmx.fr>
2018-05-18 14:53:40 +0200

    Supprission de la méthode showListClient, + créatio nde la page loginBad

--- ClientProxiBanque/src/main/java/fr/gtm/pbsi/controller/EmployeBean.java
+++ ClientProxiBanque/src/main/java/fr/gtm/pbsi/controller/EmployeBean.java
@@ -1,5 +1,6 @@
 package fr.gtm.pbsi.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -12,39 +13,49 @@ import fr.gtm.pbsi.service.EmployeService;
 @ManagedBean(name = "employebean")
 @SessionScoped
 public class EmployeBean {
+	// Déclaration des variables locales
 	private Employe employe = new Employe();
 	private EmployeService serviceEmploye = new EmployeService();
 
+	//=====================Assesseurs ======================
 	public Employe getEmploye() {
 		return employe;
 	}
 
 	public void setEmploye(Employe employe) {
 		this.employe = employe;
-	}
+	}//===========================================
+
 
 	public String loginVerification() {
 		String forward = null;
-System.out.println("début de méthode");
-		this.employe=serviceEmploye.loginVerification(this.employe);
+		System.out.println("début de méthode");
+		//this.employe = serviceEmploye.loginVerification(this.employe);
+		//=======================================================
+		this.employe.setTypeFunction(1);
+		this.employe.setId(1);
+		ArrayList<Customer> maList  = new ArrayList<Customer>() ;
+		maList.add(new Customer("toto", "totoFamilly"));
+		maList.add(new Customer("tata", "tataFamilly"));
+		this.employe.setListCustomer(maList);
+		//=======================================================
 		
 		// On test si l'employe a été trouvé en base de donnée
-		if(employe.getId()>0) {
+		System.out.println(this.employe);
+		if (employe.getId() > 0) {
+			
+			// 0 => gerant; 1 => conseiller
+			if (employe.getTypeFunction() == 0) {
+				forward = "employeList";
+			} else {
+				forward = "customerList";
+			}
+			return forward;
+		}
 		
-		// 0 => gerant; 1 => conseiller
-		if (employe.getTypeFunction() == 0) {
-			forward = "employeList";
-		} else {
-			forward = "customerList";
-		}
-		}
-
-		return forward;
-
+		
+		return "homeLoginBad";
+		
 	}
-	
-	public List<Customer> showCustomers() {
-		List<Customer> customerList = service.findAllCustomers(this.employe);
-		return customerList;
-	}
+
 }
--- ClientProxiBanque/src/main/webapp/index.jsp
+++ ClientProxiBanque/src/main/webapp/index.jsp
@@ -7,7 +7,7 @@
 </head>
 <body>
 
- <jsp:forward page="/faces/customerCreation.xhtml"></jsp:forward> 
+ <jsp:forward page="/faces/homeLogin.xhtml"></jsp:forward> 
 
 </body>
 </html>
