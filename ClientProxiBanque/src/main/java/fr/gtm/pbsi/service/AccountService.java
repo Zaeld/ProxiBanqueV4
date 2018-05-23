47aa9fb573681ead6e1df9907e57b64699cc6ef0
poste 110 <110@gtm.com>
2018-05-23 13:27:56 +0200

    correction transaction

--- ClientProxiBanque/src/main/java/fr/gtm/pbsi/service/AccountService.java
+++ ClientProxiBanque/src/main/java/fr/gtm/pbsi/service/AccountService.java
@@ -25,7 +25,7 @@ public class AccountService {
 		List<Account> accountList = null;
 		try {
 
-			WebResource webResource = client.resource("http://localhost:8080/webServiceProxiBanque/account/isActive");
+			WebResource webResource = client.resource("http://localhost:8080/webServiceProxiBanque/account/");
 
 			ClientResponse reponse = webResource.accept("application/json").get(ClientResponse.class);
 
@@ -46,7 +46,6 @@ public class AccountService {
 	public Account readAccount(Integer idAccount) {
 
 		String output = null;
-		Account account = null;
 		try {
 
 			WebResource webResource = client
@@ -56,12 +55,12 @@ public class AccountService {
 
 			output = reponse.getEntity(String.class);
 
-			account = mapper.readValue(output, Account.class);
+			return mapper.readValue(output, Account.class);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return account;
+		return null;
 	}
 	
 	/**
@@ -141,9 +140,18 @@ public class AccountService {
 	 */
 	public Transaction createTransaction(Float amountTransaction, Integer idDebitAccount, Integer idCreditAccount) {
 System.out.println("Création d'une transaction");
-		Account debitAccount = this.readAccount(idDebitAccount);
-		Account creditAccount = this.readAccount(idCreditAccount);
-		Transaction transaction = new Transaction(amountTransaction, debitAccount, creditAccount);
+//SavingAccount savingAccountDebit;
+//SavingAccount savingAccountCredit;
+//CurrentAccount CurrentAccountDebit;
+//CurrentAccount CurrentAccountCredit;
+//
+//if(this.readAccount(idDebitAccount).getNumberAccount().charAt(0)=='d') {
+//	
+//	
+//}
+//		Account debitAccount = this.readAccount(idDebitAccount);
+//		Account creditAccount = this.readAccount(idDebitAccount);
+		Transaction transaction = new Transaction(amountTransaction, idDebitAccount, idDebitAccount);
 		System.out.println("retour d'une transaction");
 
 		return transaction;
